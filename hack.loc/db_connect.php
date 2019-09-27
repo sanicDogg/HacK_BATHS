dbc963a2fce7093d449eb2c32d02e28f8102e305
KesuTain <brodin.sokol@yandex.ru>
2019-09-27 21:47:23 +0300

    Нах?й всё

--- hack.loc/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-//подключаем библиотеку redbeanphp
-require "libs/rb.php";
-//Подключаемся к базе
-R::setup( 'mysql:host=mysite.loc;dbname=students',
-        'root', '123' ); //for both mysql or mariaDB
-
-//запуск сессиии
-session_start();
