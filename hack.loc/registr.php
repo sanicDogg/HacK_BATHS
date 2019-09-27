d5e5e1e430ba741a31e041c8a5f9d9cc7c543a6c
KesuTain <brodin.sokol@yandex.ru>
2019-09-27 20:34:09 +0300

    2

--- hack.loc/registr.php
+++ hack.loc/registr.php
@@ -1,15 +1,7 @@
 <?php
 	include 'header.php';
-echo '<!doctype html>
-<html>
-	<div class="colorlib-shop">
-		<div class="container-fluid">
-			<div class="row">
-				<div> qge12444444444444444444 </div>
-			</div>
-		</div>
-	</div>
-</html>';
-	include 'footer.php';
 ?>
-
+<div></div>
+<?php
+	include 'footer.php'
+?>
