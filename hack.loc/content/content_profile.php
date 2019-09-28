b31cee358c2338ea681f99052d82c44e726fb926
Lani-Ka-Nani <vm.gainullin@gmail.com>
2019-09-29 01:31:53 +0300

    stg

--- hack.loc/content/content_profile.php
+++ hack.loc/content/content_profile.php
@@ -1,24 +1,25 @@
 <?php
-if (!empty($_SESSION['logged_user'])) {
 $id = $_SESSION['logged_user']->id;
 $user = R::load('volunteers', $id);
+if (empty($user)) {
+  echo "<p>empty</p>";
 }
 ?>
-
 <section id="home" class="video-hero" style="height: 500px; background-image: url(images/cover_img_1.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
 		<div class="overlay"></div>
 			<div class="display-t display-t2 text-center">
 				<div class="display-tc display-tc2">
 					<div class="container">
-                    <div class="col-md-6 col-md-offset-0">
-          <div class="animate-box">
-            <p><a href="vibor_volunteer.php" class="btn btn-primary btn-lg btn-custom">Я волонтер</a></p>
-          </div>
-        </div>
 						<div class="col-md-12 col-md-offset-0">
 							<div class="animate-box">
 								<h2>Мой профиль</h2>
 								<p class="breadcrumbs"><span><a href="index.php">Домой</a></span> <span>Профиль</span></p>
+
+                <div class="col-md-12 col-md-offset-0">
+                   <div class="animate-box">
+                       <p><a href="vibor_volunteer.php" class="btn btn-primary btn-lg btn-custom">Выйти из профиля</a></p>
+                   </div>
+                </div>
 							</div>
 						</div>
 					</div>
@@ -26,10 +27,6 @@ $user = R::load('volunteers', $id);
 			</div>
 </section>
 
-<?
-//Проверка на авторизованность пользователя
-if (!empty($_SESSION['logged_user'])):
-?>
 <div style="margin: 300px" class="colorlib-services colorlib-bg-white">
   <div class="container">
     <div class="row">
@@ -73,42 +70,7 @@ if (!empty($_SESSION['logged_user'])):
         </div>
       </div>
       <?endif;?>
-      <div class="col-md-4 text-center animate-box fadeInUp animated-fast">
-        <div class="services">
-          <div class="col-md-3 text-center animate-box">
-          <div class="staff-entry">
-            <a href="#" class="staff-img" style="background-image: url(images/person4.jpg);"></a>
-            <div class="desc">
-              <h3>Dorothy Murphy</h3>
-              <span>Designer</span>
-              <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              <p>
-                <ul class="colorlib-social-icons">
-                  <li><a href="#"><i class="icon-twitter"></i></a></li>
-                  <li><a href="#"><i class="icon-facebook"></i></a></li>
-                </ul>
-              </p>
-            </div>
-          </div>
-        </div>
 
-        </div>
-      </div>
     </div>
   </div>
 </div>
-
-<?else:?>
-
-<div style="" class="colorlib-services colorlib-bg-white">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4 text-center animate-box fadeInUp animated-fast">
-        <div class="services">
-            <p>Вы не авторизованы!!!</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<?endif;?>
