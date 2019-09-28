e4390291efd4f7c5dec50978645f9680b0ff0212
sanicDogg <boradach200@mail.ru>
2019-09-29 02:33:29 +0300

    summary

--- hack.loc/db_connect.php
+++ hack.loc/db_connect.php
@@ -6,6 +6,4 @@ R::setup( 'mysql:host=localhost;dbname=hack_db',
         'root', '' ); //for both mysql or mariaDB
 
 //запуск сессиии
-if (session_status() == PHP_SESSION_NONE) {
-  session_start();
-}
+session_start();
