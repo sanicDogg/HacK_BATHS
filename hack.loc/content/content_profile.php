719bd2b4528fce18866a6826fa349cde84937df7
sanicDogg <boradach200@mail.ru>
2019-09-29 00:49:04 +0300

    Теперь профиль отображается корректно

--- hack.loc/content/content_profile.php
+++ hack.loc/content/content_profile.php
@@ -5,8 +5,23 @@ if (empty($user)) {
   echo "<p>empty</p>";
 }
 ?>
+<section id="home" class="video-hero" style="height: 500px; background-image: url(images/cover_img_1.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
+		<div class="overlay"></div>
+			<div class="display-t display-t2 text-center">
+				<div class="display-tc display-tc2">
+					<div class="container">
+						<div class="col-md-12 col-md-offset-0">
+							<div class="animate-box">
+								<h2>Мой профиль</h2>
+								<p class="breadcrumbs"><span><a href="index.html">Home</a></span> <span>Services</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+</section>
 
-<div style="margin: 100px" class="colorlib-services colorlib-bg-white">
+<div style="" class="colorlib-services colorlib-bg-white">
   <div class="container">
     <div class="row">
       <? if (empty($_SESSION['logged_user']->access_token)):?>
