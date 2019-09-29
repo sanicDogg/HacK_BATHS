a247033b49f1c43646d04abfd1edf477cc4f6a94
sanicDogg <boradach200@mail.ru>
2019-09-29 05:32:04 +0300

    Update content_profile.php

--- hack.loc/content/content_profile.php
+++ hack.loc/content/content_profile.php
@@ -1,7 +1,13 @@
 <?php
 if (!empty($_SESSION['logged_user'])) {
-	$id = $_SESSION['logged_user']->id;
-	$user = R::load('volunteers', $id);
+	//Определяем, волонтер ли вошел или организация
+	if ($_SESSION['logged_user']->isVolunteer == 0) {
+		$id = $_SESSION['logged_user']->id;
+		if ($id == 1) $organization = R::load('organizations', $id);
+	} else {
+		$id = $_SESSION['logged_user']->id;
+		$user = R::load('volunteers', $id);
+	}
 }
 
 if ($_GET["logout"] == 1) {
@@ -73,11 +79,75 @@ body{background:url(http://bootstraptema.ru/images/bg/bg-1.png)}
 <?
 //Проверка на авторизованность пользователя
 if (!empty($_SESSION['logged_user'])):
-?>
+ //Если организация вошла
+			if (!empty($organization)):
+			?>
+			<section>
+			    <div class="overlay"></div>
+			      <div class="display-t display-t2 text-center">
+			        <div class="display-tc display-tc2">
+			          <div class="container">
+			            <div class="col-md-12 col-md-offset-0">
+			              <div class="animate-box">
+			                <h2>Волонтеры</h2>
+			                <p class="breadcrumbs"><span><a href="index.php">Домой</a></span> <span>Карточки волонтеров</span></p>
+			                            <div class="col-md-12 col-md-offset-0">
+			              <div class="animate-box">
+			                <p><a href="register_event.php" class="btn btn-primary btn-lg btn-custom">Создать событие</a></p>
+			              </div>
+			            </div>
+			              </div>
+			            </div>
+			          </div>
+			        </div>
+			      </div>
+			    </section>
+
+			    <div class="colorlib-blog">
+			      <div class="container">
+			        <div class="row">
+			          <div class="col-md-4 text-center animate-box">
+			            <div class="staff-entry">
+			              <a href="https://vk.com/idlani_ka_nani" class="staff-img" style="background-image: url(images/pxkKrN3vsCQ.jpg);"></a>
+			                <div class="desc">
+			                 <h3>Виталий Гайнуллин</h3>
+			                <span>IT евангелист, дизайнер</span>
+			                <p>Парень, хорошо работающий языком и это не то, о чем вы возможно подумали.</p>
+			                </div>
+			            </div>
+			          </div>
+			                  <div class="col-md-4 text-center animate-box">
+			          <div class="staff-entry">
+			            <a href="#" class="staff-img" style="background-image: url(images/2QC-qIJeYH4.jpg);"></a>
+			            <div class="desc">
+			              <h3>Александр Молитвин</h3>
+			              <span>Камитан, программист</span>
+			              <p>Небольшой мальчуган-гений. Написал сайт стоящий 20к в одиночку и не попросил денег.</p>
+			            </div>
+			           </div>
+			          </div>
+			        </div>
+
+			        <div class="row">
+			          <div class="col-md-12 text-center">
+			            <ul class="pagination">
+			              <li class="disabled"><a href="#">&laquo;</a></li>
+			              <li class="active"><a href="#">1</a></li>
+			              <li><a href="#">2</a></li>
+			              <li><a href="#">3</a></li>
+			              <li><a href="#">4</a></li>
+			              <li><a href="#">&raquo;</a></li>
+			            </ul>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+			<? //Если вошел без ВК
+			elseif (empty($_SESSION['logged_user']->access_token)):
+			?>
 <div class="colorlib-services colorlib-bg-white" style="padding: 0">
   <div class="container">
     <div class="row">
-      <? if (empty($_SESSION['logged_user']->access_token)):?>
       <div class="col-md-4 text-center animate-box fadeInUp animated-fast">
         <div class="services">
           <p>
@@ -89,8 +159,9 @@ if (!empty($_SESSION['logged_user'])):
           </p>
         </div>
       </div>
-			<?endif;?>
-      <? if (!empty($_SESSION['logged_user']->access_token)):
+      <? //Если вошел через ВК
+				elseif (!empty($_SESSION['logged_user']->access_token)):
+
 				$access_token = $user->access_token;
 				$vk = new \VK\Client\VKApiClient();
 				$response = $vk->users()->get($access_token, array());
@@ -209,7 +280,9 @@ if (!empty($_SESSION['logged_user'])):
 </div>
 </div>
 </div>
-			<?endif;?>
+<?
+	endif;
+?>
 
 <?else:?>
 
